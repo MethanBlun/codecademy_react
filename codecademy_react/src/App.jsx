832e255635d90bd960c8d18b93dcb78a968deffe
Osirus Jack <methanblun@gmail.com>
2024-03-05 23:19:01 +0000

    className attribut

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,24 +1,25 @@
 
-
-// function App() {
-
-
-//   return (
-//     <>
-    
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+const bigDiv = <div className='bigDiv'>I AM A BIG ONE</div>
+function App() {
 
-const container = document.getElementById('container');
-const root = createRoot(container);
-root.render(<h1>Hello world</h1>);
+
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default App
+
+
+
+
+// const container = document.getElementById('container');
+// const root = createRoot(container);
+// root.render(<h1>Hello world</h1>);
 
