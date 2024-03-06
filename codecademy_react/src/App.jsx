4a8b6eef1840f8d91f5895ca1624e352261bd5cc
Osirus Jack <methanblun@gmail.com>
2024-03-06 01:11:05 +0000

    JSX Conditionals: &&

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,26 +1,28 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
-const bigDiv = <div className='bigDiv'>I AM A BIG ONE</div>
-const theBestString = 'This text was accessed through a javascript variable';
-let drinkingAge = 100
-let age = 10
-const headline = (
-    <h1>
-
-      { age >= drinkingAge ? 'Buy Drink' : 'Do Teen Stuff' }
-    </h1>
+const judgmental = Math.random() < 0.5;
+const favoriteFoods = (
+    <div>
+      <h1>My Favorite Foods</h1>
+      <ul>
+        <li>Sushi Burrito</li>
+        <li>Rhubarb Pie</li>
+        { !judgmental &&
+      <li>Nacho Cheez Straight Out The Jar</li>
+        }
+        <li>Broiled Grapefruit</li>
+      </ul>
+    </div>
   );
+
 function App() {
 
 
   return (
     <>
 
-    {bigDiv}
-    <h1>{theBestString}</h1>
-    {headline}
+   {favoriteFoods}
 
     </>
   )
