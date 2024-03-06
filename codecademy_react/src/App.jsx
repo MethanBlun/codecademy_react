207219869c679f7b1640c4ed72ffc29195126e50
Osirus Jack <methanblun@gmail.com>
2024-03-06 01:03:12 +0000

    ternary operation

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -4,14 +4,24 @@ import { createRoot } from 'react-dom/client';
 
 const bigDiv = <div className='bigDiv'>I AM A BIG ONE</div>
 const theBestString = 'This text was accessed through a javascript variable';
+let drinkingAge = 100
+let age = 10
+const headline = (
+    <h1>
+
+      { age >= drinkingAge ? 'Buy Drink' : 'Do Teen Stuff' }
+    </h1>
+  );
 function App() {
 
 
   return (
     <>
+
     {bigDiv}
     <h1>{theBestString}</h1>
-    
+    {headline}
+
     </>
   )
 }
