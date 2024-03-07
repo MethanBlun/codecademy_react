38383a4a4bdf0b35de9237974088f23037f1bb9d
Osirus Jack <methanblun@gmail.com>
2024-03-07 13:35:28 +0000

    map() function for array

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,29 +1,22 @@
 
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-const judgmental = Math.random() < 0.5;
-const favoriteFoods = (
-    <div>
-      <h1>My Favorite Foods</h1>
-      <ul>
-        <li>Sushi Burrito</li>
-        <li>Rhubarb Pie</li>
-        { !judgmental &&
-      <li>Nacho Cheez Straight Out The Jar</li>
-        }
-        <li>Broiled Grapefruit</li>
-      </ul>
-    </div>
-  );
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleList = people.map((person,i) =>
+
+  <li>{person}</li>
+
+);
+
 
 function App() {
 
 
   return (
     <>
-
-   {favoriteFoods}
-
+    llll
+    <ul>{peopleList}</ul>
     </>
   )
 }
@@ -33,7 +26,4 @@ export default App
 
 
 
-// const container = document.getElementById('container');
-// const root = createRoot(container);
-// root.render(<h1>Hello world</h1>);
 
