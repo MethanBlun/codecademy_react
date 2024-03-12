d50a77b0aed6312b1239dea73c710074782a2721
Osirus Jack <methanblun@gmail.com>
2024-03-12 07:00:24 +0000

    rendering owl in other file

--- codecademy_react/src/main.jsx
+++ codecademy_react/src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Owl from './owl.jsx'
 import './index.css'
 import './test.scss'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Owl/>
   </React.StrictMode>,
 )
