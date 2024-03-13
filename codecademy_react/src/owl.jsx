1e036dd72d6b5940257f429983e1b234cb698dab
Osirus Jack <methanblun@gmail.com>
2024-03-13 15:04:14 +0000

    handle function learned

--- codecademy_react/src/owl.jsx
+++ codecademy_react/src/owl.jsx
@@ -7,10 +7,18 @@ const owl = {
   title: "Excellent Owl",
   src: "https://content.codecademy.com/courses/React/react_photo-owl.jpg",
 };
-
+function ButtonSi() {
+    function handleclick(){
+      return alert('fuck !! i was clicked')
+    }
+    return <button onClick={handleclick}> dont click here
+    </button>
+}
 
 function Owl() {
   return (
+    <>
+  
     <div>
       <h1>{owl.title}</h1>
       <img 
@@ -20,6 +28,10 @@ function Owl() {
       alt = {owl.title} 
       />
     </div>
+    <ButtonSi/>
+    
+
+      </>
   );
 }
 
