dcdd1e04ee49ab4e7d599ab103d0c62e1b532b4a
Osirus Jack <methanblun@gmail.com>
2024-03-24 09:50:19 +0000

    grocery exercice done

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,15 +1,16 @@
 import React, { createElement } from "react";
 import { createRoot } from "react-dom/client";
 import Student from "./student";
+import GroceryItem from "./student";
 // import CryptoJS from 'crypto-js';
 
 function App() {
-  return (<>
- 
-  <Student age={13} name="moustapha" isStudentRich={true} />
-  <Student age={27}  isStudentRich={true} />
-   </>
-  )
+  const handleClick = () => {
+    alert('yen a plus')
+  }
+  return (
+   <GroceryItem name='kk' onClick={handleClick} />
+  );
 }
 
 export default App;
