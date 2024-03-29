0d4ca081bd10475d22db9297e5b0e25540822794
Osirus Jack <methanblun@gmail.com>
2024-03-29 18:56:40 +0000

    use map function for display fruits array

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,27 +1,29 @@
-import React, { createElement } from "react";
+import React, { createElement, useState } from "react";
 import { createRoot } from "react-dom/client";
 
 // import CryptoJS from 'crypto-js';
 
+//afficher une liste de fruit avec map
 
+// const [compteur, setCompteur] = useState(1);
 
-
-   const myArray = [1,2,3,4,5,6,7,8]
-
-const doubleNumeber = myArray.map(n => n*2)
+const fruits = [
+  { id: 1, nom: "dakkar" },
+  { id: 2, nom: "mango" },
+  { id: 3, nom: "xaal" },
+  { id: 4, nom: "maad" },
+];
 
 function App() {
-  
-
   return (
     <>
-
-
-  {myArray}
-  <hr />
-  {doubleNumeber}
+      <div>
+        <ul>{fruits.map((fruit) => {
+          return <li>{fruit.nom}</li>
+        })}</ul>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
