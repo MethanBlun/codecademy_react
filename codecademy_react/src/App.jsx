a90c6da73c95518ea9c7988b43f7004f966d4ce2
Osirus Jack <methanblun@gmail.com>
2024-04-07 12:24:35 +0000

    stolen brick of code for next project (winframe)

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const array = ['']
 
 function App() {
   const [selected, setSelected] = useState([]);
 
   return (
     <>
-      <p>
-        {arr.reduce(function (acc, curr) {
-          return curr + acc;
-        }, 0)}
+ 
 
 
-
-{/* const sum = arr.reduce(function (acc, curr) {
-  return acc + curr;
-}, 0); */}
-      </p>
     </>
   );
 }
