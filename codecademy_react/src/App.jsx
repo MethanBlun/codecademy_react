68486a920e8e9e2658154ee7d5263c49fbc8c03b
Osirus Jack <methanblun@gmail.com>
2024-04-09 13:13:42 +0000

    first use of useEffect hook

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -2,32 +2,37 @@ import React, { useEffect, useState } from "react";
 
 
 
+function AlertExample() {
+  const [showAlert, setShowAlert] = useState(true);
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowAlert(false);
+    }, 5000); // Le composant restera monté pendant 5 secondes
+
+    return () => {
+      clearTimeout(timeout);
+      alert('Le composant sera démonté !');
+    };
+  }, []);
+
+  return (
+    <div>
+      {showAlert && <h1>Le composant est monté !</h1>}
+      {!showAlert && <h1>Le composant est démonté !</h1>}
+    </div>
+  );
+}
 
 
 
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [firstname, setFirstname] = useState("");
-  useEffect(()=>{},[])
+  
 
   return (
     <>
-      <div>
-        <input 
-        type="text" 
-        value={title} 
-        onChange={setTitle} 
-        />
-
-<input 
-        type="text" 
-        value={firstname} 
-        onChange={setFirstname} 
-        />
-
-      </div>
+<AlertExample/>
     </>
   );
 }
