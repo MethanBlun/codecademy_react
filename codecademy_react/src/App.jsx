9a37b0adaa35349ebdd74896b0dc4e2dfc251f24
Osirus Jack <methanblun@gmail.com>
2024-04-12 18:07:17 +0000

    welcome message

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -3,16 +3,19 @@ import React, { useEffect, useState } from "react";
 function PageTitle() {
   const [clickCount, setClickCount] = useState(0);
 
-  const increment = () => {
-    setClickCount((prev) => prev + 1);
-  };
+  // const increment = () => {
+  //   setClickCount((prev) => prev - 0.01);
+  // };
   useEffect(() => {
-    document.addEventListener("keydown", increment);
-    return document.removeEventListener("keydown", increment)
-  });
+    alert('welcome to the brand new website , realise your dream , work everyday ')
+    // document.addEventListener("keydown", increment);
+    // alert(clickCount)
+    // return document.removeEventListener("keydown", increment)
+  },[]);
 
-  
-  return (
+    
+
+  return (    
     <div>
       <h1>Document Clicks: {clickCount}</h1>
     </div>
