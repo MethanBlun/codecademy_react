14870c2ee23fb155559e3076bcb6bb8f244a9281
Osirus Jack <methanblun@gmail.com>
2024-04-13 12:03:19 +0000

    timer finnaly works

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,15 +1,46 @@
+// import React, { useEffect, useState } from "react";
+
+// function Timer() {
+//   const [time, setTime] = useState(0);
+
+//   useEffect(() => {
+//     let intervalId = setInterval(() => {
+//       setTime((prev) => prev + 1);
+//     }, 1000);
+
+//     return () => clearInterval(intervalId);
+//   }, []); // tableau de dépendances vide pour s'assurer que useEffect ne se déclenche qu'une seule fois
+
+//   return (
+//     <>
+//       <h1>Time: {time}</h1>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <>
+//       <Timer />
+//     </>
+//   );
+// }
+
+// export default App;
+
 import React, { useEffect, useState } from "react";
 
 function Timer() {
   const [time, setTime] = useState(0);
-  useEffect(() => {
-    setInterval(()=>{setTime((prev)=> prev + 1)},2000)
-  },[]);
 
-  //  const intervalId = setInterval(() => {setTime ((prev)=> prev + 1);})
-  // function intervalId(){
-  //   setInterval(()=>{setTime((prev) => prev + 1)})
-  // }
+  useEffect(() => {
+    let intervalId = setInterval(() => {
+      setTime((prev) => prev + 1);
+    }, 1000);
+    console.log(time);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <>
       <h1>Time: {time}</h1>
