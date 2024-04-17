800d8fd1cdb25768acbe0c27676065ca420ddb3f
Osirus Jack <methanblun@gmail.com>
2024-04-17 17:55:33 +0000

    try to pick pokemon from input

--- codecademy_react/src/App.jsx
+++ codecademy_react/src/App.jsx
@@ -1,65 +1,86 @@
-// import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-// function Timer() {
-//   const [time, setTime] = useState(0);
-//   const [name, setName] = useState("");
-//   const handlechange = ({ target }) => {
-//     setName(target.value);
-//   };
+// fetch(`https://pokeapi.co/api/v2/pokemon/${choosenPokemon}`)
+// .then(resp => {
+//   if (!resp.ok){
+//     throw new Error('could not fetch ressources')
+//   }
+// })
 
-//   useEffect(() => {
-//     let intervalId = setInterval(() => {
-//       setTime((prev) => prev + 1), console.log("conmponent is mounted");
-//     }, 1000);
 
-//     return () => clearInterval(intervalId), console.log("component unmounted");
-//   }, []);
-// <p></p>
-//   return (
-//     <>  
-//       <input type="text" onChange={handlechange} />
-//       <h1>Time: {time}</h1>
-//     </>
-//   );
-// }
+// fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+//     .then(response => {
+//         if(!response.ok){
+            
+//             throw new Error('could not fetch ressources')
+//         }
+//      return response.json()
+//     })
+//     .then(data => console.log(data.id))
+//     .catch(error => console.error(error))
 
-// function App() {
-//   return (
-//     <>
-     
-//       <Timer />
-//     </>
-//   );
-// }
 
-// export default App;
+function CheckPokemon() {
+  const [choosenPokemon, setChoosenPokemon] = useState(""); 
 
-// App.js
-import React from 'react';
-import './App.css';
+  const CheckIfPekemon = ({ target }) => {
+    const pokemon = target.value;
+    setChoosenPokemon(pokemon);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${choosenPokemon}`)
+.then(resp => {
+  if (!resp.ok){
+    throw new Error('could not fetch ressources')
+  }
+})
 
-function App() {
+  };
+
   return (
-    <div className="App">
-      <header>
-        <div className="container mx-auto">
-          <h1 className="text-2xl">Citations inspirantes</h1>
-          {/* Ajoute ici ton système d'affichage de citations */}
-        </div>
-      </header>
-      
-      <main className="container mx-auto mt-8">
-        <h2 className="text-2xl mb-4">Gestionnaire de tâches</h2>
-        {/* Ajoute ici la liste de tâches */}
-      </main>
-
-      <footer>
-        <div className="container mx-auto">
-          <p>&copy; 2024 Gestionnaire de tâches</p>
-        </div>
-      </footer>
+    <div>
+      <p>type your favorite pokemon</p>
+      <input onChange={CheckIfPekemon}></input>
+      <p>you choose : {choosenPokemon}</p> {/* Affichage de l'état */}
     </div>
   );
 }
 
+function App() {
+  return (
+    <>
+      <CheckPokemon />
+    </>
+  );
+}
+
 export default App;
+
+
+// // App.js
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header>
+//         <div className="container mx-auto">
+//           <h1 className="text-2xl">Citations inspirantes</h1>
+//           {/* Ajoute ici ton système d'affichage de citations */}
+//         </div>
+//       </header>
+
+//       <main className="container mx-auto mt-8">
+//         <h2 className="text-2xl mb-4">Gestionnaire de tâches</h2>
+//         {/* Ajoute ici la liste de tâches */}
+//       </main>
+
+//       <footer>
+//         <div className="container mx-auto">
+//           <p>&copy; 2024 Gestionnaire de tâches</p>
+//         </div>
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default App;
